3f5768ceea01d30c717c624ceb1998d5e550a3cb
Q <q@q-continuum.net>
2013-07-24 07:47:38 -0600

    wx_lang, not wx_locale. fuck

--- i18n_core/gui.py
+++ i18n_core/gui.py
@@ -5,7 +5,7 @@ import i18n_core
 
 def set_wx_locale(locale_path, domain, locale_id):
  wx_lang = wx.Locale.FindLanguageInfo(locale_id)
- if wx_locale is None:
+ if wx_lang is None:
   logger.warning("Unable to find wx locale %s, falling back to default." % locale_id)
   wx_lang = wx.Locale.FindLanguageInfo(locale_id)
   wx_lang = wx.Locale.FindLanguageInfo(i18n_core.DEFAULT_LOCALE)
