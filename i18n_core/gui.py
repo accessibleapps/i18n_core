a8f785460b54190685e47a9307b3a323b3907298
Q <q@q-continuum.net>
2013-07-24 07:46:30 -0600

    If wx can't find the locale, have it use the default en_US locale

--- i18n_core/gui.py
+++ i18n_core/gui.py
@@ -1,7 +1,14 @@
+from logging import getLogger
+logger = getLogger('i18n_core.gui')
 import wx
+import i18n_core
 
 def set_wx_locale(locale_path, domain, locale_id):
  wx_lang = wx.Locale.FindLanguageInfo(locale_id)
+ if wx_locale is None:
+  logger.warning("Unable to find wx locale %s, falling back to default." % locale_id)
+  wx_lang = wx.Locale.FindLanguageInfo(locale_id)
+  wx_lang = wx.Locale.FindLanguageInfo(i18n_core.DEFAULT_LOCALE)
  wx_locale = wx.Locale(wx_lang.Language)
  wx_locale.AddCatalogLookupPathPrefix(locale_path)
  wx_locale.AddCatalog(domain)
