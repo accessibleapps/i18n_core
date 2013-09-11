144930eb0c1cb8c9f3b8ccde272ea8c75d37860e
Christopher Toth <q.alpha@gmail.com>
2013-09-10 18:39:54 -0600

    Removed redundant line

--- i18n_core/gui.py
+++ i18n_core/gui.py
@@ -7,7 +7,6 @@ def set_wx_locale(locale_path, domain, locale_id):
  wx_lang = wx.Locale.FindLanguageInfo(locale_id)
  if wx_lang is None:
   logger.warning("Unable to find wx locale %s, falling back to default." % locale_id)
-  wx_lang = wx.Locale.FindLanguageInfo(locale_id)
   wx_lang = wx.Locale.FindLanguageInfo(i18n_core.DEFAULT_LOCALE)
  wx_locale = wx.Locale(wx_lang.Language)
  wx_locale.AddCatalogLookupPathPrefix(locale_path)
