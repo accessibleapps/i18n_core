87dfccd175a2f2a80ba8515d4e2ceecba9db1c44
Q <q@q-continuum.net>
2014-03-21 16:20:56 -0600

    Handle locale ids with . in them in the gui module at least

--- i18n_core/gui.py
+++ i18n_core/gui.py
@@ -1,14 +1,16 @@
 from logging import getLogger
 logger = getLogger('i18n_core.gui')
+import locale
+
 import wx
 import i18n_core
+import babel.core
 
 def set_wx_locale(locale_path, domain, locale_id):
+ locale_id = locale_id.split('.')[0]
  wx_lang = wx.Locale.FindLanguageInfo(locale_id)
  wx.Locale.AddCatalogLookupPathPrefix(locale_path)
  wx_locale = wx.Locale()
  wx_locale.AddCatalog('wxstd')
  wx_locale.Init(wx_lang.Language)
  return wx_locale
-
-
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = 0.3
+__version__ = 0.31
 __author__ = 'Christopher Toth <q@q-continuum.net>'
 __doc__ = """Internationalization and localization setup and support utilities."""
 
