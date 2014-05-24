611e244bb895763f2a645292879c62f6cf21e832
Christopher Toth <q.alpha@gmail.com>
2014-05-24 15:39:57 -0600

    Moved the code which finds a wx lang into its own function. support locales split by underscore for wx locales, this can be less precise but might be better?

--- i18n_core/gui.py
+++ i18n_core/gui.py
@@ -7,8 +7,7 @@ import i18n_core
 import babel.core
 
 def set_wx_locale(locale_path, domain, locale_id):
- locale_id = locale_id.split('.')[0]
- wx_lang = wx.Locale.FindLanguageInfo(locale_id)
+ wx_lang = find_wx_lang(locale_id)
  if wx_lang is None:
   logger.warning("No wx translation found for locale %s" % locale_id)
   return
@@ -17,3 +16,15 @@ def set_wx_locale(locale_path, domain, locale_id):
  wx_locale.AddCatalog('wxstd')
  wx_locale.Init(wx_lang.Language)
  return wx_locale
+
+def find_wx_lang(locale_id):
+ locale_id = locale_id.split('.')[0]
+ wx_lang = wx.Locale.FindLanguageInfo(locale_id)
+ if wx_lang is not None:
+  logger.debug("Perfect match: Found wx locale for %s" % locale_id)
+  return wx_lang
+ locale_id = locale_id.split('_')[0]
+ wx_lang = wx.Locale.FindLanguageInfo(locale_id)
+ if wx_lang is not None:
+  logger.warn("Secondary fallback: Found wx locale for %s" % locale_id)
+  return wx_lang
