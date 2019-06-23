85f93ccf9fa2b60b7c93080af8c9a8855a1dbbd4
Christopher Toth <q.alpha@gmail.com>
2019-06-22 22:39:21 -0600

    Bumped version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-__version__ = 0.33
-__author__ = "Christopher Toth <q@q-continuum.net>"
+__version__ = 0.34
 __doc__ = """Internationalization and localization setup and support utilities."""
 
 
 setup(
     name="i18n_core",
     version=str(__version__),
+    author="Christopher Toth",
+    author_email="q@q-continuum.net",
     description=__doc__,
     packages=["i18n_core"],
     install_requires=["babel", "platform_utils"],
