8d68deb15780c9fbded916eca0b866dd701dc11b
Christopher Toth <q.alpha@gmail.com>
2020-12-08 23:01:29 -0700

    Remove old undocumented monkeypatch which broke newer versions of pytz

--- i18n_core/patches.py
+++ i18n_core/patches.py
@@ -1,18 +1,15 @@
 from platform_utils import paths
 import pickle
-import babel.core
 
 if paths.is_frozen():
-    import pytz
+    import babel.core
 
-    pytz.resource_exists = lambda name: False
     with open(
         os.path.join(paths.embedded_data_path(), "babel", "global.dat"), "rb"
     ) as fp:
         babel.core._global_data = pickle.load(fp)
 
-import babel.localedata
+    import babel.localedata
 
-if paths.is_frozen():
     babel.localedata._dirname = os.path.join(paths.embedded_data_path(), "locale-data")
 
